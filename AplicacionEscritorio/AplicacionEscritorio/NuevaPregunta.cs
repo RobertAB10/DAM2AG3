f3ea1b087f313d1829d6cbd2ee8d256b323226f7
RobertAB10 <45283806+RobertAB10@users.noreply.github.com>
2019-11-06 22:03:44 +0100

    Add files via upload

---
+++ AplicacionEscritorio/AplicacionEscritorio/NuevaPregunta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionEscritorio
+{
+    public partial class NuevaPregunta : Form
+    {
+        public NuevaPregunta()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            MaximizeBox = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NuevaPregunta_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonRespuestaCorrecta_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPregunta_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
