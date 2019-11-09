6ebbb3cc59012a17c33d264434c3a85e029b99d9
RobertAB10 <45283806+RobertAB10@users.noreply.github.com>
2019-11-09 20:39:33 +0100

    PantallaInicial

--- AplicacionEscritorio/AplicacionEscritorio/Modificar Pregunta.Designer.cs
+++ AplicacionEscritorio/AplicacionEscritorio/Modificar Pregunta.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Modificar_Pregunta));
             this.label5 = new System.Windows.Forms.Label();
             this.comboBoxTema = new System.Windows.Forms.ComboBox();
             this.pictureBoxPregunta = new System.Windows.Forms.PictureBox();
@@ -326,7 +327,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::AplicacionEscritorio.Properties.Resources.Universo;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(744, 531);
             this.Controls.Add(this.pictureBoxIdioma);
