45cee6d37b3317d6937a33c702beeb7fe24b06dc
RobertAB10 <45283806+RobertAB10@users.noreply.github.com>
2019-11-10 18:13:29 +0100

    JSON

--- AplicacionEscritorio/AplicacionEscritorio/NuevaPregunta.cs
+++ AplicacionEscritorio/AplicacionEscritorio/NuevaPregunta.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ namespace AplicacionEscritorio
 {
     public partial class NuevaPregunta : Form
     {
+        List<Pregunta> preguntas = new List<Pregunta>();
+       
         public NuevaPregunta()
         {
             InitializeComponent();
@@ -25,9 +29,25 @@ namespace AplicacionEscritorio
             comboBoxNivel.Items.AddRange(Constants.Nivells);
         }
 
-        private void radioButtonRespuestaCorrecta_CheckedChanged(object sender, EventArgs e)
+        private string rutaIdioma(object sender, EventArgs e)
         {
+            string ruta = "";
+            String Idioma = comboBoxIdiomes.Text;
+            switch (Idioma)
+            {
+                case "Català":
+                    ruta = @"..\..\Resources\JSON\preguntesCAT.json";
+                    break;
 
+                case "Castellano":
+                    ruta = @"..\..\Resources\JSON\preguntesES.json";
+                    break;
+
+                case "English":
+                    ruta = @"..\..\Resources\JSON\preguntesEN.json";
+                    break;
+            }
+            return ruta;
         }
 
         private void pictureBoxPregunta_Click(object sender, EventArgs e)
@@ -67,6 +87,43 @@ namespace AplicacionEscritorio
         private void buttonCancela_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void buttonConfirma_Click(object sender, EventArgs e)
+        {
+            if (textBoxPregunta.Text == "" || comboBoxNivel.Text == "" || comboBoxTema.Text == "") {
+                MessageBox.Show("Faltan campos por completar", "INFORMACIÓN", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+                Pregunta pregunta = new Pregunta();
+                pregunta.pregunta = textBoxPregunta.Text;
+                pregunta.imagen = pictureBoxPregunta.ImageLocation;
+                pregunta.nivell = comboBoxNivel.Text;
+                pregunta.respuestas = new List<Respuesta>();
+
+                //Por acabar
+                //
+
+                //Cargamos el JSON del idioma
+                string ruta = rutaIdioma(sender, e);
+                if (System.IO.File.Exists(ruta))
+                {
+                    //MessageBox.Show("El fichero existe");
+                    JArray jArrayPeliculas = JArray.Parse(File.ReadAllText(ruta));
+                    preguntas = jArrayPeliculas.ToObject<List<Pregunta>>();
+                }
+                else
+                {
+                    //MessageBox.Show("Fichero creado");
+                    StreamWriter fichero = File.CreateText(ruta);
+                }
+            }
+
+        }
+
+        private void NuevaPregunta_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //guardarFichero();
         }
     }
 }
